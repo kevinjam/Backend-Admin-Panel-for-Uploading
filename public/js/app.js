b881b10a562d3b82758253a3d827fd8a6782d4b9
Kevin Janvier <kevin.janvier5@gmail.com>
2018-04-03 00:40:38 +0300

    add category validation

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,38 @@
-
-  // Initialize Firebase
-    var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyCL49AU6K69UCTmvD-lyXQ8Y2F_UDB06Ok",
     authDomain: "mytithe-b25bc.firebaseapp.com",
     databaseURL: "https://mytithe-b25bc.firebaseio.com",
     projectId: "mytithe-b25bc",
     storageBucket: "mytithe-b25bc.appspot.com",
     messagingSenderId: "2612091569"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
 firebase.auth.Auth.Persistence.LOCAL;
 
-$("#btn-login").click(function(){
-	var email = $("#email").val();
-	var password =$("#password").val();
+$("#btn-login").click(function () {
+    var email = $("#email").val();
+    var password = $("#password").val();
 
-	var result =firebase.auth().signInWithEmailAndPassword(email, password);
-	result.catch(function(error){
+    var result = firebase.auth().signInWithEmailAndPassword(email, password);
+    result.catch(function (error) {
 
-		var errorCode = error.code;
-		var errorMessage = error.message;
+        var errorCode = error.code;
+        var errorMessage = error.message;
 
-		console.log(errorCode);
-		console.log(errorMessage);
-	})
+        console.log(errorCode);
+        console.log(errorMessage);
+    });
 });
+
+function switchView(view) {
+    $.get({
+        url: view,
+        cache: false,
+
+    }).then(function (data) {
+        $("#container").html(data);
+    });
+
+}
